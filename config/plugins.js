7bef37051fbb3659cafbcf0acbbeb4e106d21570
nilspineda <nilspineda@gmail.com>
2023-12-06 22:00:19 -0500

    update

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+    "vercel-deploy": {
+      enabled: true,
+      config: {
+        deployHook:
+          "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+        apiToken: "8rKiPIolgJX9H0myR8Q9T0NF",
+        appFilter: "strapi-cd-2",
+        teamFilter: "nilspineda",
+        roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+      },
+    },
+  });
